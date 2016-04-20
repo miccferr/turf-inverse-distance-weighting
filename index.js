e4a8c53cda5cd690466901af0c2b0bbd6657ad2e
Michele Ferretti <mferretti@Micheles-iMac.local>
2016-04-20 14:25:06 +0100

    alg. calculates IDW for each point in a set

--- index.js
+++ index.js
@@ -3,40 +3,27 @@ var fs = require('fs');
 var randomPoints = fs.readFileSync('./data/data.geojson');
 randomPoints = JSON.parse(randomPoints);
 
-// console.log(JSON.stringify(randomPoints));
-
-target = {
-      "type": "Feature",
-      "properties": {
-        "marker-color": "#7e7e7e",
-        "marker-size": "medium",
-        "marker-symbol": "",
-        "value = 1": ""
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          -50.44921875,
-          58.63121664342478
-        ]
-      }
-    }
-
 function IDW(Z,b) {
 
-  zw = 0
-  sw = 0
-  N = Z.length
-  Z.map(function (e) {
-    d = turf.distance(e, target, 'kilometers')
-    if (d == 0) {
-      return e.properties.value;
-    }
-    w = 1.0/Math.pow(d,b);
-    sw += w;
-    zw += w * e.properties.value
-  })
-  return zw/sw
-}
+  N = Z.length;
+  idw_arr = [];
 
-console.log(IDW(randomPoints.features,0.5));
+  for (i=0; i<N; i++) {
+    zw = 0;
+    sw = 0;
+
+    Z.map(function (point) {
+      d = turf.distance(Z[i], point, 'kilometers')
+      console.log(d);
+      if (d == 0) {
+        return point.properties.value;
+      }
+      w = 1.0/Math.pow(d,b);
+      sw += w;
+      zw += w * point.properties.value
+    })
+    Z[i].properties.idw = zw/sw;
+  }
+
+  return idw_arr
+}
