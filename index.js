2bf727f8ccee3bf4f05d3d8d82c4057da46a99fa
Michele Ferretti <mferretti@Micheles-iMac.local>
2016-04-20 19:33:06 +0100

    refactored code. import only ad hoc turf modules

--- index.js
+++ index.js
@@ -1,43 +1,45 @@
-var turf = require('turf');
-var fs = require('fs');
-// var testPoints = fs.readFileSync('./data/data.geojson');
-// testPoints = JSON.parse(testPoints);
+var distance = require('turf-distance');
+var squareG = require('turf-square-grid');
+var centroid = require('turf-centroid');
+var extent = require('turf-extent');
 
-// IDW takes in input
-// controlPoints = sampled points dataset
-// b = IDW power parameter
-// cellWidth = output grid cell size
-// units = miles or kilometers
-module.exports = function (controlPoints, b,cellWidth, units) {
+/**
+* Takes a set of known points, a power parameter, a cell depth, a unit of measurement
+* and returns a set of square polygons in a grid with a property IDW for each cell
+*
+* [description]
+* @param  {FeatureCollection<Point>} controlPoints Sampled points at known locations
+* @param  {Number} b             Exponent to alter the distance weight
+* @param  {Number} cellWidth     The distance across each cell
+* @param  {String} units         Used in calculating cellWidth ('miles' or 'kilometers')
+*/
+module.exports =  function (controlPoints, b, cellWidth, units) {
 
-   // create sampling point grid Z
-  var extent = turf.extent(controlPoints);
-  Z = turf.pointGrid(extent, cellWidth, units);
-  // create a square grid to visualize the output (like a raster..)
-  var squareGrid = turf.squareGrid(turf.extent(Z), cellWidth, units);
+  // create a sample square grid
+  // compared to a point grid helps visualizing the output (like a raster..)
+  var squareGrid = turf.squareGrid(turf.extent(controlPoints), cellWidth, units);
 
-  N = Z.features.length;
+  N = squareGrid.features.length;
   idw_arr = [];
 
   // for every sampling point..
-  for (i=0; i<N; i++) {
+  for (i = 0; i < N; i++) {
     zw = 0;
     sw = 0;
-    // calculate the distance and idw to each control point
+    // calculate the distance from each control point to cell's centroid
     controlPoints.features.map(function (point) {
-      d = turf.distance(Z.features[i], point, units)
+        d = turf.distance(turf.centroid(squareGrid.features[i]), point, units)
 
-      if (d == 0) {
-        zw = point.properties.value;
-        return
-      }
-      w = 1.0/Math.pow(d,b);
-      sw += w;
-      zw += w * point.properties.value
-    })
-    Z.features[i].properties.idw = zw/sw;
-    squareGrid.features[i].properties.z = Z.features[i].properties.idw
-    console.log(Z.features[i]);
+        if (d == 0) {
+          zw = point.properties.value;
+          return
+        }
+        w = 1.0 / Math.pow(d, b);
+        sw += w;
+        zw += w * point.properties.value
+      })
+      // write IDW value for each cell
+    squareGrid.features[i].properties.z = zw / sw
   }
 
   return squareGrid;
