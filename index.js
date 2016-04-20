419de0ebb8d4dc9dd282ca2712a016614b3824ef
Michele Ferretti <mferretti@Micheles-iMac.local>
2016-04-20 16:43:53 +0100

    added smaller test dataset + alg. now returns IDW on point grid

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
 var turf = require('turf');
 var fs = require('fs');
-var randomPoints = fs.readFileSync('./data/data.geojson');
-randomPoints = JSON.parse(randomPoints);
+var testPoints = fs.readFileSync('./data/data.geojson');
+testPoints = JSON.parse(testPoints);
 
-function IDW(Z,b) {
+// IDW takes in input
+// Z = grid points dataset
+// b = IDW power
+// cellWidth = output grid cell size
+// units = miles or kilometers
 
-  N = Z.length;
+function IDW(controlPoints, b,cellWidth, units) {
+
+   // create sampling point grid Z
+  var extent = turf.extent(controlPoints);
+  Z = turf.pointGrid(extent, cellWidth, units);
+
+  N = Z.features.length;
   idw_arr = [];
 
+  // for every sampling point..
   for (i=0; i<N; i++) {
     zw = 0;
     sw = 0;
+    // calculate the distance and idw to each control point
+    controlPoints.features.map(function (point) {
+      d = turf.distance(Z.features[i], point, units)
 
-    Z.map(function (point) {
-      d = turf.distance(Z[i], point, 'kilometers')
-      console.log(d);
       if (d == 0) {
-        return point.properties.value;
+        zw = point.properties.value;
+        return
       }
       w = 1.0/Math.pow(d,b);
       sw += w;
       zw += w * point.properties.value
     })
-    Z[i].properties.idw = zw/sw;
+    Z.features[i].properties.idw = zw/sw;
+    console.log(Z.features[i]);
   }
+  // console.log(Z);
+  fs.writeFileSync('./output.geojson', JSON.stringify(Z));
 
-  return idw_arr
+
 }
+
+IDW(testPoints,0.5,3,'kilometers')
